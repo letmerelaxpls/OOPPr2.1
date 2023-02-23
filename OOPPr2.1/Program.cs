d078726d6575fceacef6fd73d76201ff3c2c1d00
Georgiy <larsubivec@gmail.com>
2023-02-23 18:08:02 +0400

    first commit

---
+++ OOPPr2.1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+
+class Student
+{
+    public string Name { get; set; }
+    public int Grade { get; set; }
+
+    public Student(string name, int grade)
+    {
+        Name = name;
+        Grade = grade;
+    }
+}
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        string fileName = "students.txt";
+
+
+        List<Student> students = new List<Student>();
+        using (StreamReader sr = new StreamReader(fileName))
+        {
+            string line;
+            while ((line = sr.ReadLine()) != null)
+            {
+                string[] parts = line.Split(',');
+                string name = parts[0].Trim();
+                int grade = int.Parse(parts[1].Trim());
+                students.Add(new Student(name, grade));
+            }
+        }
+
+
+        var filteredStudents = students.Where(s => s.Grade >= 4);
+        Console.WriteLine("Result of filtration by grades: ");
+
+        foreach (var student in filteredStudents)
+        {
+            Console.WriteLine("{0}: {1}", student.Name, student.Grade);
+        }
+    }
+}
+
+
